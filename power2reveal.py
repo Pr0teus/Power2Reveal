f8cb7d056e39f9c1b9f1c27dbbeb0e9684405d90
Pr0teus <felipeaesposito@gmail.com>
2018-02-18 23:14:05 -0300

    init coding

---
+++ power2reveal.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import pptx
+import click
+
+
+class Power2Reveal(object):
+    """docstring for Power2Reveal"""
+    def __init__(self, arg):
+        super(Power2Reveal, self).__init__()
+        self.arg = arg
+
+
+if __name__ == '__main__':
+    Power2Reveal()
